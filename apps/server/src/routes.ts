45ab54cd28c2a9f5717f9630bd19aecc6ab56afd
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-07-22 02:09:58 -0300

    feat: router

---
+++ apps/server/src/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+import { Cadastro } from './controller/Empresa.Controller';
+
+export const router = Router();
+const controllerCadastro = new Cadastro();
+
+router.post('/cadastro/', controllerCadastro.create);
