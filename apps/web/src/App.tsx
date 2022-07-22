188b07196e7c8da3d23bc9b1e73568f7444eb8d7
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-07-22 14:34:56 -0300

    feat: react-router-dom e context

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CadastroStorage } from "./context/use-cadastro-context";
+import { Cadastro } from "./pages/cadastro/Cadastro";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
-
-
   return (
-    <div>teste</div> 
-  )
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <CadastroStorage>
+          <Routes>
+            <Route path="/" element={<Cadastro />} />
+          </Routes>
+        </CadastroStorage>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
