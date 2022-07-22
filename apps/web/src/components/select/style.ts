167c1da4e1854c93f4974c5b13b42e7e8a4ed608
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-07-22 14:21:29 -0300

    feat: component select e interface

---
+++ apps/web/src/components/select/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const LabelStyle = styled.label`
+  display: block;
+`;
+
+export const ContainerSelect = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+export const SelectStyle = styled.select`
+  width: 5rem;
+  height: 2rem;
+  border-radius: 0.25rem;
+  border: 1px solid #d7d7d7;
+  background: #e7e9ee;
+  font-weight: 400;
+`;
+export const ErrorStyle = styled.p`
+  font-size: 12px;
+  margin-top: 0.5rem;
+  margin-left: 0.7rem;
+  color: red;
+`;
